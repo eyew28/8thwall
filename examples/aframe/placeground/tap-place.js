547ecf0d5d4a9f8c7c144f79812dc48a060cb8a8
eyew-xrg <eyew@redcartel.com.au>
2026-03-21 17:05:10 +1100

    remove random rotation

--- examples/aframe/placeground/tap-place.js
+++ examples/aframe/placeground/tap-place.js
@@ -13,8 +13,8 @@ AFRAME.registerComponent('tap-place', {
       const touchPoint = event.detail.intersection.point
       newElement.setAttribute('position', touchPoint)
 
-      const randomYRotation = Math.random() * 360
-      newElement.setAttribute('rotation', `0 ${randomYRotation} 0`)
+      // const randomYRotation = Math.random() * 360
+      // newElement.setAttribute('rotation', `0 ${randomYRotation} 0`)
 
       newElement.setAttribute('visible', 'false')
       newElement.setAttribute('scale', '0.0001 0.0001 0.0001')
@@ -31,7 +31,7 @@ AFRAME.registerComponent('tap-place', {
         newElement.setAttribute('visible', 'true')
         newElement.setAttribute('animation', {
           property: 'scale',
-          to: '7 7 7',
+          to: '3 3 3',
           easing: 'easeOutElastic',
           dur: 800,
         })
